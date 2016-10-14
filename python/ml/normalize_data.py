a78caeaaa8632ce2f4a5c78464343be6ed731b1a
cunxin.wang <cunxin.wang@gewara.com>
2016-10-14 18:36:02 +0800

    commit update

--- python/ml/normalize_data.py
+++ python/ml/normalize_data.py
@@ -2,8 +2,38 @@
 
 __author__ = 'kevin'
 
-if __name__ == '__main__':
 
+def normalize1(X):
+    from sklearn import preprocessing
+    # scale the data attributes
+    scaled_X = preprocessing.scale(X)
+    print scaled_X
+
+    # normalize the data attributes
+    normalized_X = preprocessing.normalize(X)
+    print normalized_X
+
+
+def normalize2(X):
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    scaler.fit(X)  # Don't cheat - fit only on training data
+
+    X_train = scaler.transform(X)
+    print X_train
+
+
+def normalize3(X):
+    from sklearn.grid_search import GridSearchCV
+    from sklearn.linear_model import SGDClassifier
+    parameters = {'alpha': [0.1, 1, 10]}
+
+    sgd = SGDClassifier()
+    clf = GridSearchCV(sgd, parameters)
+    clf.fit(X, y)
+    print clf
+
+if __name__ == '__main__':
     # 1. 加载数据(Data Loading)
 
     import numpy as np
@@ -19,18 +49,14 @@ if __name__ == '__main__':
     y = dataset[:, 8]
 
     # 2. 数据归一化(Data Normalization)
+    # scale
+    #normalize1(X)
 
-    from sklearn import preprocessing
-    # scale the data attributes
-    scaled_X = preprocessing.scale(X)
+    # StandardScaler
+    #normalize2(X)
 
-    # normalize the data attributes
-    normalized_X = preprocessing.normalize(X)
-    print normalized_X
-
-    # standardize the data attributes
-    standardized_X = preprocessing.scale(X)
-    print standardized_X
+    # cv
+    normalize3(X)
 
     raw_data.close()
 
--- python/ml/optimise_parameter.py
+++ python/ml/optimise_parameter.py
@@ -2,8 +2,40 @@
 
 __author__ = 'kevin'
 
-if __name__ == '__main__':
 
+def optimise1(X, y):
+
+    from sklearn.linear_model import Ridge
+    from sklearn.grid_search import GridSearchCV
+    # prepare a range of alpha values to test
+    alphas = np.array([1, 0.1, 0.01, 0.001, 0.0001, 0])
+    # create and fit a ridge regression model, testing each alpha
+    model = Ridge()
+    grid = GridSearchCV(estimator=model, param_grid=dict(alpha=alphas))
+    grid.fit(X, y)
+    print(grid)
+    # summarize the results of the grid search
+    print(grid.best_score_)
+    print(grid.best_estimator_.alpha)
+
+def optimise2(X, y):
+
+    from scipy.stats import uniform as sp_rand
+    from sklearn.linear_model import Ridge
+    from sklearn.grid_search import RandomizedSearchCV
+    # prepare a uniform distribution to sample for the alpha parameter
+    param_grid = {'alpha': sp_rand()}
+    # create and fit a ridge regression model, testing random alpha values
+    model = Ridge()
+    rsearch = RandomizedSearchCV(estimator=model, param_distributions=param_grid, n_iter=100)
+    rsearch.fit(X, y)
+    print(rsearch)
+    # summarize the results of the random parameter search
+    print(rsearch.best_score_)
+    print(rsearch.best_estimator_.alpha)
+
+
+if __name__ == '__main__':
     # 1. 加载数据(Data Loading)
 
     import numpy as np
@@ -19,34 +51,11 @@ if __name__ == '__main__':
     y = dataset[:, 8]
 
     # 5. 优化算法参数
-    #选择正确的参数：用搜索的方法来确定参数：正则参数选择
+    # 选择正确的参数：用搜索的方法来确定参数：正则参数选择
 
-    import numpy as np
-    from sklearn.linear_model import Ridge
-    from sklearn.grid_search import GridSearchCV
-    # prepare a range of alpha values to test
-    alphas = np.array([1,0.1,0.01,0.001,0.0001,0])
-    # create and fit a ridge regression model, testing each alpha
-    model = Ridge()
-    grid = GridSearchCV(estimator=model, param_grid=dict(alpha=alphas))
-    grid.fit(X, y)
-    print(grid)
-    # summarize the results of the grid search
-    print(grid.best_score_)
-    print(grid.best_estimator_.alpha)
+    optimise1(X, y)
 
-    #随机从给定区间中选择参数是很有效的方法，然后根据这些参数来评估算法的效果进而选择最佳的那个
-    import numpy as np
-    from scipy.stats import uniform as sp_rand
-    from sklearn.linear_model import Ridge
-    from sklearn.grid_search import RandomizedSearchCV
-    # prepare a uniform distribution to sample for the alpha parameter
-    param_grid = {'alpha': sp_rand()}
-    # create and fit a ridge regression model, testing random alpha values
-    model = Ridge()
-    rsearch = RandomizedSearchCV(estimator=model, param_distributions=param_grid, n_iter=100)
-    rsearch.fit(X, y)
-    print(rsearch)
-    # summarize the results of the random parameter search
-    print(rsearch.best_score_)
-    print(rsearch.best_estimator_.alpha)
+
+    # 随机从给定区间中选择参数是很有效的方法，然后根据这些参数来评估算法的效果进而选择最佳的那个
+
+    # optimise2(X, y)
