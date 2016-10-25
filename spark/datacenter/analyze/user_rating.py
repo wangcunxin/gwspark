c2a268f460d2a58eedb50821df8e6d0d22ee8bdb
cunxin.wang <cunxin.wang@gewara.com>
2016-10-25 10:42:58 +0800

    commit  update

--- spark/datacenter/analyze/user_rating.py
+++ spark/datacenter/analyze/user_rating.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import datetime, time, sys
 import numpy as np
 
 from sklearn.preprocessing import StandardScaler
@@ -12,17 +13,18 @@ __author__ = 'kevin'
 sep = ','
 sep2 = "_"
 
-if __name__ == '__main__':
+
+def main(argv):
     try:
         # 1.load and filter
         hBaseClient = HBaseClient()
         table = "up_dat"
-        dat = "201608"
+        dat = argv[0]
         cfs = ['DF']
         rs = hBaseClient.scanByPrefix(table, dat, cfs)
         qualifiers = "buy_times,buy_quantity,amount,discount_amount,discount_times," \
                      "days,buy_cinemas,buy_cities,avg_time,avg_count".split(",")
-        print 'rs:len', rs.__len__()
+        print 'load rs:len', len(rs)
         lines = []
         for r in rs:
             rowkey = r[0].row.split('#')[1]
@@ -32,7 +34,7 @@ if __name__ == '__main__':
                 avg_count = tmp.value
 
             if avg_count == 0 or rowkey == '55859667':
-                print 'filter:special users: rowkey=%s,avg_count=%s' % (rowkey, avg_count)
+                # print 'filter:special users: rowkey=%s,avg_count=%s' % (rowkey, avg_count)
                 continue
 
             arr = []
@@ -46,9 +48,11 @@ if __name__ == '__main__':
                 arr.append(value)
             lines.append(sep.join(arr))
 
-        print 'lines:len', lines.__len__()
+        print 'filter lines:len', len(lines)
 
         # 2.scale features
+        if len(lines) == 0:
+            return
         dataset = np.loadtxt(lines, delimiter=",")
         x1 = dataset[:, 1:len(lines) + 1]
         y1 = dataset[:, 0]
@@ -76,7 +80,7 @@ if __name__ == '__main__':
             rating.append(ele)
 
         # 4.dump to mango
-        print 'rating:len', rating.__len__()
+        print 'rate rating:len', rating.__len__()
         conf_file = "../../../userprofile/config-mongodb.properties"
         prop = Properties()
         conf = prop.getProperties(conf_file)
@@ -89,6 +93,21 @@ if __name__ == '__main__':
         mongodbClient = MongodbClient(host, port, dbname, username, password)
         colName = "user_rating"
         mongodbClient.setCollection(colName)
-        col = mongodbClient.insertMany(rating)
+        mongodbClient.insertMany(rating)
+        print 'dump finish'
+
     except Exception, e:
-        print e
+        print 'exception happened:', e
+
+
+if __name__ == '__main__':
+    begin = time.time()
+    if len(sys.argv) != 2:
+        print 'user_rating.py <ym>'
+        sys.exit(-1)
+    print sys.argv
+    main(sys.argv[1:])
+    end = time.time()
+    now = datetime.datetime.now()
+    dat = now.strftime("%Y-%m-%d %H:%M:%S")
+    print dat, 'total cost', round(end - begin, 3), 's'
--- userprofile/properties.py
+++ userprofile/properties.py
@@ -2,27 +2,28 @@ import platform
 
 __author__ = 'wangcx'
 
+
 class Properties(object):
     def __init__(self):
         pass
 
     def getProperties(self, fileName):
+        properties = {}
         try:
             pro_file = open(fileName, 'r')
             try:
                 os = platform.system().lower()
-                sep='\r\n'
-                if(os.__eq__("windows")):
-                    sep='\r\n'
-                elif(os.__eq__("linux")):
-                    sep='\n'
-                properties = {}
+                sep = '\n'
+                # is equal or ==
+                if os == "windows":
+                    sep = '\r\n'
+                elif os == "linux":
+                    sep = '\n'
+
                 for line in pro_file:
                     if line.find('=') > 0:
                         strs = line.replace(sep, '').split('=')
                         properties[strs[0]] = strs[1]
-            except Exception, e1:
-                print e1
             finally:
                 pro_file.close()
         except Exception, e:
